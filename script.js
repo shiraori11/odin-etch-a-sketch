f99ec80699d86a568646d76a6b231156a8c9605b
shiraori11 <shiraori@proton.me>
2026-05-22 20:41:45 +0800

    Added functional rainbow coloring and opacity I managed to add random coloring with functional opacity. I think that I did a pretty good job even with little experience. I think solving programming problems from leetcode, codewars, or other website would be beneficial to my growth.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const sketchCanvasContainer = document.querySelector(".etch-a-sketch-container")
-
 const generateCanvasRow = function(canvasSize) {
   const rowContainer = document.createElement("div");
   rowContainer.classList.add("row-canvas");
   for (let r = 0; r < canvasSize; r++) {
     const canvasBox = document.createElement("div");
-    canvasBox.addEventListener("mouseover", changeColorOnHover);
     canvasBox.classList.add("canvas");
+    canvasBox.addEventListener("mouseover", changeColorOnHover);
     rowContainer.append(canvasBox);
   }
-
   return rowContainer;
 }
 
@@ -25,9 +22,39 @@ const generateCanvasColumn = function(canvasSize) {
 }
 
 const changeColorOnHover = function(event) {
-  event.target.style.backgroundColor = "black";
+
+  const currentBgColor = event.target.style.backgroundColor
+
+  const randomBgColor = generateRandomColor();
+
+  if (currentBgColor === "") {
+    event.target.style.backgroundColor = randomBgColor;
+  }
+  
+  const addOpacity = parseFloat(event.target.style.opacity) + 0.10
+  
+  if (isNaN(addOpacity)) {
+    event.target.style.opacity = 0.10;
+  } else {
+    event.target.style.opacity = addOpacity;
+  }
+  
+  console.log(addOpacity);
 }
 
+const generateRandomColor = function() {
+  let randomColor = "";
+
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  randomColor = `rgb(${r}, ${g}, ${b})`;
+
+  return randomColor;
+}
+
+const sketchCanvasContainer = document.querySelector(".etch-a-sketch-container")
 const sketchCanvas = generateCanvasColumn(4);
 const headerDiv = document.createElement("div");
 const footerDiv = document.createElement("div");
